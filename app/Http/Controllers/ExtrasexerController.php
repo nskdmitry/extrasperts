7ffefe2b1e0ca07c83954f88b58759802f47e906
nskdmitry <dmitry.m-ko2014@yandex.ru>
2021-09-25 17:49:30 +0700

    Extrasexer, if it was asked, say number as answer what took from astral (balda).

---
+++ app/Http/Controllers/ExtrasexerController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Divination;
+use Illuminate\Http\Request;
+
+class ExtrasexerController extends Controller
+{
+    private static $astral = [
+        "90 + rand(0, 9)",
+        "min(rand(10, 99), time() % 100)",
+        "rand(0, 9)*10 + rand(0, 9)",
+        "100 - rand(1, 99)",
+        "rand(10, 84)",
+        "time() % 90 + 10",
+        "54",
+    ];
+
+    //
+    public function divination($id_person, $id_wish)
+    {
+        return Divination::create([
+            'id_extra' => $id_person,
+            'id_history' => $id_wish,
+            'number' => eval(static::$astral[$id_person % count(static::$astral)])
+        ]);
+    }
+}
