d26b6341018589795bfd4b57296ca466ef635930
nskdmitry <dmitry.m-ko2014@yandex.ru>
2021-09-25 19:28:03 +0700

    Some routes of API functions was declared in api.php file.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ExtrasexerController;
+use App\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,20 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::post('/wish', function (Request $request) {
+    $user_helper = new UserController();
+    $expert_helper = new ExtrasexerController();
+
+    $login = $request->post('login');
+    $email = $request->post('email');
+    $user = User::where('email', 'LIKE', "'{$email}'")->where("login", "LIKE", "'{$login}'")->first()
+        ?? $user_helper->anoname($request);
+    $wish = $user_helper->initWish(new Request([], ['uid' => $user->id]));
+
+    $extrasexers = \App\Extrasexer::all(["id"]);
+    foreach ($extrasexers as $extrasexer) {
+        $expert_helper->divination($extrasexer->id, $wish->id);
+    }
+    redirect()->route("/user/wish/{$wish->id}/answer");
 });
