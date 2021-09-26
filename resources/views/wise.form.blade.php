5b24e8b292cc14a5473eac357fc5ab7c5a7bb198
nskdmitry <dmitry.m-ko2014@yandex.ru>
2021-09-26 22:15:38 +0700

    Form of start divination from extrasexers.

---
+++ resources/views/wise.form.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <form action="{{route('/user/wise')}}" method="post">
+        <label for="login">Ваше имя: </label>
+            <input class="flex-center" type="text" name="login" placeholder="Представьтесь, пожалуйста"/><br/>
+        <label for="email">Ваша почта<sub>*</sub></label>
+            <input class="flex-center" type="email" name="email" placeholder="Ваш e-mail (обязательно)" required /><br/>
+        <button class="flex-center" type="submit">Загадали?<br/>Жмите!</button>
+    </form>
+</div>
