c50c93eda6cc56c7cf21d24ecaba49c9e51d48a3
nskdmitry <dmitry.m-ko2014@yandex.ru>
2021-09-26 22:27:13 +0700

    List oitem of extrasexer history.

---
+++ resources/views/extra.story.blade.php
@@ -0,0 +1,7 @@
+<li><div>
+        {{$case->userCase->created}}. Пользователь {{$case->userCase->user->name}} загадал число {{$case->userCase->number}}.
+        И это было предсказано {{$case->created}}: {{$case->telling}}!
+        @if(!$case->trueth)
+            <sub class="hidden_error">Промашка вышла. Забудьте. Забудьте, вам сказано!</sub>
+        @endif
+    </div></li>
