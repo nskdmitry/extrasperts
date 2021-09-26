478e3ddc311f64a3e7a38e913beff5dd0e412a17
nskdmitry <dmitry.m-ko2014@yandex.ru>
2021-09-26 22:15:00 +0700

    Rewrite a start page to our purpose.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,40 +61,28 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+            .text {
+                font-size: 16px;
+                font-family: 'Nunito', sans-serif;
+                font-weight: normal;
+                text-decoration: none;
+                color: black;
+            }
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Битва экстрасексов
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="text">Добро пожаловать, дорогой посетитель. Вы не верите в дремлющие в человеке сверхспособности! Тогда вы по адресу!
+                    <br />Наши мощные сверхчувственные, собранные со всех концов нашей необъятной родины, вернут вам забутую в детстве веру в чудеса!
+                    <br />Не верите? А давайте проверим!
                 </div>
             </div>
+            @include('wise.form')
         </div>
     </body>
 </html>
