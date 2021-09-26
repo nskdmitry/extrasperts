d481b317ecd0ac0acd2b3126fbc597d948bbed2f
nskdmitry <dmitry.m-ko2014@yandex.ru>
2021-09-26 15:26:04 +0700

    New property of UserStory: user.

--- app/UserStorie.php
+++ app/UserStorie.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class UserStorie extends Model
 {
     //
+    public function user()
+    {
+        return $this->belongsTo("users", "id_user", "id");
+    }
 }
