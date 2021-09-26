a6d791fbe2f8c5904dfbed969d5e9a9f5b0e9551
nskdmitry <dmitry.m-ko2014@yandex.ru>
2021-09-27 00:25:31 +0700

    User profile (history of wishes) page template.

--- resources/views/user.history.blade.php
+++ resources/views/user.history.blade.php
@@ -1,1 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    @include('handler', ["title" => "Дайте ответ!"])
+</head>
+<body>
+    <div class="flex-center position-ref">
+        <h4>История загадок от пользователя {{$user->name}}</h4>
+        @each('wish.item', $wishes, 'wish')
+    </div>
+</body>
+</html>
