11b134357ed9733b4b2c266442533e81374d8998
nskdmitry <dmitry.m-ko2014@yandex.ru>
2021-09-27 09:07:24 +0700

    Add names to routes of web pages.

--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,17 @@ Route::get('/', function () {
 });
 Route::get('/expert', function() {
 
-});
+})->name('experts');
 Route::get('/expert/{id}', function ($id) {
     $person = Extrasexer::findOrFail($id);
     view('extrasexer.profile', ['person' => $person, 'history' => $person->history]);
-});
-Route::get('/user/{id}/profile', 'UserController@profile');
+})->name('expert');
+Route::get('/user/{id}/profile', 'UserController@profile')->name('user');
 Route::get('/user/wish', function() {
     view('welcome');
-});
+})->name('wish');
 Route::get("/user/wish/{id}/answer", function ($id) {
     $divinations = Divination::where("id_history", "=", $id)->all();
     view('answer', ['id' => $id, 'tries' => $divinations, 'user' => $divinations[0]->userCase->user]);
-});
+})->name('answer');
 
